8c5ad3522792b5c34b3122abb4f92d2a21cd0755
saatvika kondepudi <101541781+Saatvikakondepudi@users.noreply.github.com>
2022-05-31 13:51:00 +0000

    counting letter with output

--- lettercount.c
+++ lettercount.c
@@ -11,3 +11,7 @@ int main(void) {
   printf("\nThe number of letters in the given sentence are : %d\n", number_of_letters);
   return 0;
 }
+/*output:-
+Enter a sentence: happy
+
+The number of letters in the given sentence are : 5*/
